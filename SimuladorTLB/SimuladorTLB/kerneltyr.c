577727754e0a7c9d4d9aba6a009fcfd81373d9de
Pablo Guardado <pabloguardadoalvarez@gmail.com>
2019-12-18 20:35:40 +0100

    Winter is commit

---
+++ SimuladorTLB/SimuladorTLB/kerneltyr.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define MAX_LINE 100
+
+int main(){
+ printf("hola");
+	int pipe;
+	char line[MAX_LINE];
+
+int c;
+FILE *file;
+file = fopen("accesos_memoria.txt", "r");
+if (file) {
+    while ((c = getc(file)) != EOF)
+        printf(c);
+    fclose(file);
+    }
+return 0;
+}   
